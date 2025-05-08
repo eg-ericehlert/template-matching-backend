faea4d1a6f59be87233656e84f77a96bbcddabb0
eric <ehehlert1994@gmail.com>
2025-05-08 14:08:16 -0700

    added flask cors

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-# app.py
-# Simple Flask application that returns "hello" at the /hello route
-
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)   # <-- this opens all origins; you can lock it down if you like
 
 @app.route('/hello')
 def hello():
     return "hello"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
