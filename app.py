56dbc4e38992744d855bf347686793f6979034c9
eric <ehehlert1994@gmail.com>
2025-05-10 11:35:29 -0700

    cleaned up app.py

--- app.py
+++ app.py
@@ -23,57 +23,6 @@ def test():
 def health():
     return '', 200
 
-
-
-# @app.route('/match_template', methods=['POST'])
-# def match_template():
-#     data = request.get_json(force=True)
-#     input_dir = data.get('dir')
-#     base_file = data.get('base_filename')
-
-#     if not input_dir or not base_file:
-#         return jsonify({"error": "Both 'dir' and 'base_filename' are required"}), 400
-
-#     # Download the base file from S3
-#     download_images(bucket_name=s3_bucket_name, prefix=input_dir, local_base=input_dir, 
-#                             s3_key=os.getenv('AWS_ACCESS_KEY_ID'), 
-#                             s3_secret=os.getenv('AWS_SECRET_ACCESS_KEY'))
-    
-#     full_dir = os.path.abspath(input_dir)
-#     if not os.path.isdir(full_dir):
-#         return jsonify({"error": f"Directory not found: {full_dir}"}), 404
-
-#     try:
-#         result = templatematch.run_job(full_dir, base_file)
-#         results_image = os.path.join(input_dir, 'results.png')
-#         counts_image = os.path.join(input_dir, 'annotation_counts.png')
-#         upload_image(bucket_name=s3_bucket_name, local_path=results_image, 
-#                             object_key=os.path.join(input_dir, 'results.png'), 
-#                             s3_key=os.getenv('AWS_ACCESS_KEY_ID'), 
-#                             s3_secret=os.getenv('AWS_SECRET_ACCESS_KEY'))
-#         upload_image(bucket_name=s3_bucket_name, local_path=counts_image,
-#                             object_key=os.path.join(input_dir, 'annotation_counts.png'), 
-#                             s3_key=os.getenv('AWS_ACCESS_KEY_ID'), 
-#                             s3_secret=os.getenv('AWS_SECRET_ACCESS_KEY'))
-#         # Clean up local files
-#         os.remove(results_image)
-#         os.remove(counts_image)
-
-#         try:
-#             shutil.rmtree(full_dir)
-#             app.logger.info(f"Removed directory: {full_dir}")
-#         except Exception as e:
-#             app.logger.error(f"Failed to remove directory {full_dir}: {e}")
-
-#         # Return the result
-#         return jsonify(result)
-    
-#     except FileNotFoundError as fnf:
-#         return jsonify({"error": str(fnf)}), 404
-#     except Exception:
-#         app.logger.exception("Error during template matching")
-#         return jsonify({"error": "Internal server error"}), 500
-
 if __name__ == '__main__':
     # Use env vars or defaults
     host = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
